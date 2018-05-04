50f655e52c2675b3289fb2ae9a5d10190e7d9e81
wley3337@gmail.com <wley3337@gmail.com>
2018-05-04 01:14:13 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else
+  
+      katz_deli.each.with_index(1) do |x, index|
+         puts "The line is currently: #{index}. #{x}"
+      end
+  
+  end
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  puts "Welcome #{name}. You are number #{katz_deli.length} in line."
+end
