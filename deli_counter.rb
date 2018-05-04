0a93810623d56be7520386238d16bc3eee668131
wley3337@gmail.com <wley3337@gmail.com>
2018-05-04 01:57:36 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,9 +6,10 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
-      katz_deli_line_list = katz_deli
-      katz_deli_line_list.map! |x|
-        "#{katz_deli_line_list.index() + 1}. " + x
+      katz_deli_line_list = []
+      
+      katz_deli.each.with_index(1) |x, index|
+        katz_deli_line_list.push(#{index}. #{x})
       puts "The line is currently: #{katz_deli_line_list.join(", ")}"
   end
 end
